f302ff288aa7027dfb2184c9cb43d328cf130590
Drew Taylor <drew.taylor@perficient.com>
2020-06-11 11:42:31 -0400

    Added loan page

---
+++ FabrikamAspPrac1/Calculator/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FabrikamAspPrac1.Calculator
+{
+    public static class Calculator
+    {
+        public static double addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+    }
+}
