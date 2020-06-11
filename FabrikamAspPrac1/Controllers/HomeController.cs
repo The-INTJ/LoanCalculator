ba5ef514c363ec4f8d1d7a1e197ce9331340e0ed
Drew Taylor <drew.taylor@perficient.com>
2020-06-11 12:01:30 -0400

    Achieved correct MVC flow

--- FabrikamAspPrac1/Controllers/HomeController.cs
+++ FabrikamAspPrac1/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace FabrikamAspPrac1.Controllers
             return View();
         }
 
+        public ActionResult LoanRateCalculator()
+        {
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ FabrikamAspPrac1/Controllers/MathController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FabrikamAspPrac1.Controllers
+{
+    public class MathController : Controller
+    {
+        // GET: Math
+        public ActionResult LoanRateCalculator()
+        {
+            return View();
+        }
+    }
+}
